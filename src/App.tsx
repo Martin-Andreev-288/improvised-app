2733ce0e5581a136fd4a718253a4c49018321515
Martin Andreev <martin_i_a@abv.bg>
2025-10-18 20:00:09 +0300

    Create a Movie Search Application

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import MovieCard from "./components/MovieCard";
+import { type Movie } from "./types.ts";
+
+const API_URL = "https://www.omdbapi.com?apikey=YOUR_API_KEY";
+
+const App: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const searchMovies = async (title: string) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    if (data.Search) {
+      setMovies(data.Search);
+    }
+  };
+
+  useEffect(() => {
+    searchMovies("Batman"); // Initial search
+  }, []);
+
+  return (
+    <div className="app">
+      <h1 className="title">MovieLand</h1>
+
+      <div className="search-bar">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              searchMovies(searchTerm);
+            }
+          }}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="movies-container">
+          {movies.map((movie) => (
+            <MovieCard key={movie.imdbID} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { type Movie } from "../types.ts";
+
+interface MovieCardProps {
+  movie: Movie;
+}
+
+const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
+  return (
+    <div className="movie-card">
+      <img
+        src={
+          movie.Poster !== "N/A"
+            ? movie.Poster
+            : "https://via.placeholder.com/400"
+        }
+        alt={movie.Title}
+      />
+      <div className="movie-info">
+        <h3>{movie.Title}</h3>
+        <p>{movie.Year}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
